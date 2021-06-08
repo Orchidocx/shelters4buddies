7232ad32d46e43426daf68ccbf1e2855d3906e75
Jacky Li <ilikemacaronii@gmail.com>
2021-06-08 00:03:12 -0700

    filler content

--- src/App.js
+++ src/App.js
@@ -1,22 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Image from "./components/Image/Image";
+import cat from "./assets/cat.png";
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <Image src={cat} />
       </header>
     </div>
   );
