3955a7b3e042784c9c0ec288ed37971909660ce5
Jacky Li <ilikemacaronii@gmail.com>
2021-06-09 14:56:49 -0700

    added sample code for Images

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -4,7 +4,29 @@ import cat from "../../assets/cat.png";
 function App() {
   return (
     <div className="app">
+      <Image src={cat} size={ImageSize.xs} shape={ImageShape.ellipse} />
+      <Image src={cat} size={ImageSize.sm} shape={ImageShape.ellipse} />
+      <Image src={cat} size={ImageSize.md} shape={ImageShape.ellipse} />
+      <Image src={cat} size={ImageSize.lg} shape={ImageShape.ellipse} />
       <Image src={cat} size={ImageSize.xl} shape={ImageShape.ellipse} />
+      <br />
+      <Image src={cat} size={ImageSize.xs} shape={ImageShape.default} />
+      <Image src={cat} size={ImageSize.sm} shape={ImageShape.default} />
+      <Image src={cat} size={ImageSize.md} shape={ImageShape.default} />
+      <Image src={cat} size={ImageSize.lg} shape={ImageShape.default} />
+      <Image src={cat} size={ImageSize.xl} shape={ImageShape.default} />
+      <br />
+      <Image src={"cat"} size={ImageSize.xs} shape={ImageShape.default} />
+      <Image src={"cat"} size={ImageSize.sm} shape={ImageShape.default} />
+      <Image src={"cat"} size={ImageSize.md} shape={ImageShape.default} />
+      <Image src={"cat"} size={ImageSize.lg} shape={ImageShape.default} />
+      <Image src={"cat"} size={ImageSize.xl} shape={ImageShape.default} />
+      <br />
+      <Image src={"cat"} size={ImageSize.xs} shape={ImageShape.ellipse} />
+      <Image src={"cat"} size={ImageSize.sm} shape={ImageShape.ellipse} />
+      <Image src={"cat"} size={ImageSize.md} shape={ImageShape.ellipse} />
+      <Image src={"cat"} size={ImageSize.lg} shape={ImageShape.ellipse} />
+      <Image src={"cat"} size={ImageSize.xl} shape={ImageShape.ellipse} />
     </div>
   );
 }
