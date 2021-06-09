2ec67d2d1d49ab6a7b3d442d5460950280ee82c5
Jacky Li <ilikemacaronii@gmail.com>
2021-06-09 16:33:59 -0700

    changed Button from js to tsx

---
+++ src/components/Button/Button.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+type ButtonProps = {
+  name: string;
+};
+
+const Button = ({ name }: any) => (
+  <div className="button">
+    <h1>Hello button</h1>
+  </div>
+);
+
+export default Button;
