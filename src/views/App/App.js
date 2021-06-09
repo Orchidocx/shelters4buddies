445cd992b4fc43e2ed0a9f770b6d6adcfab21456
Jacky Li <ilikemacaronii@gmail.com>
2021-06-09 16:33:43 -0700

    add Home to main App

--- src/views/App/App.js
+++ src/views/App/App.js
@@ -1,32 +1,10 @@
 import "./App.css";
-import { Image, ImageShape, ImageSize } from "../../components/Image/Image.tsx";
-import cat from "../../assets/cat.png";
+
+import Home from "../Home/Home";
 function App() {
   return (
     <div className="app">
-      <Image src={cat} size={ImageSize.xs} shape={ImageShape.ellipse} />
-      <Image src={cat} size={ImageSize.sm} shape={ImageShape.ellipse} />
-      <Image src={cat} size={ImageSize.md} shape={ImageShape.ellipse} />
-      <Image src={cat} size={ImageSize.lg} shape={ImageShape.ellipse} />
-      <Image src={cat} size={ImageSize.xl} shape={ImageShape.ellipse} />
-      <br />
-      <Image src={cat} size={ImageSize.xs} shape={ImageShape.default} />
-      <Image src={cat} size={ImageSize.sm} shape={ImageShape.default} />
-      <Image src={cat} size={ImageSize.md} shape={ImageShape.default} />
-      <Image src={cat} size={ImageSize.lg} shape={ImageShape.default} />
-      <Image src={cat} size={ImageSize.xl} shape={ImageShape.default} />
-      <br />
-      <Image src={"cat"} size={ImageSize.xs} shape={ImageShape.default} />
-      <Image src={"cat"} size={ImageSize.sm} shape={ImageShape.default} />
-      <Image src={"cat"} size={ImageSize.md} shape={ImageShape.default} />
-      <Image src={"cat"} size={ImageSize.lg} shape={ImageShape.default} />
-      <Image src={"cat"} size={ImageSize.xl} shape={ImageShape.default} />
-      <br />
-      <Image src={"cat"} size={ImageSize.xs} shape={ImageShape.ellipse} />
-      <Image src={"cat"} size={ImageSize.sm} shape={ImageShape.ellipse} />
-      <Image src={"cat"} size={ImageSize.md} shape={ImageShape.ellipse} />
-      <Image src={"cat"} size={ImageSize.lg} shape={ImageShape.ellipse} />
-      <Image src={"cat"} size={ImageSize.xl} shape={ImageShape.ellipse} />
+      <Home />
     </div>
   );
 }
