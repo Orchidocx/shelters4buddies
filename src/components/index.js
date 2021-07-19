1d910e67bfb9533d50e6983fae988ac0a4c16134
Jacky Li <ilikemacaronii@gmail.com>
2021-07-18 21:58:24 -0700

    added some ways to easy import components

---
+++ src/components/index.js
@@ -0,0 +1,14 @@
+import { Image, ImageShape, ImageSize } from "./Image/Image";
+import { Button, ButtonShape, ButtonSize } from "./Button/Button";
+const Img = { Image, ImageShape, ImageSize };
+const Btn = { Button, ButtonShape, ButtonSize };
+export {
+  Image,
+  ImageShape,
+  ImageSize,
+  Button,
+  ButtonShape,
+  ButtonSize,
+  Img,
+  Btn,
+};
--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -1,5 +1,13 @@
 import "./Home.css";
-import { Image, ImageShape, ImageSize } from "../../components/Image/Image";
+import {
+  Img,
+  Image,
+  ImageShape,
+  ImageSize,
+  Button,
+  ButtonShape,
+  ButtonSize,
+} from "../../components";
 import cat from "../../assets/cat.png";
 function Home() {
   return (
@@ -11,12 +19,17 @@ function Home() {
         dolore aperiam maxime ducimus dolor vel quibusdam unde numquam nostrum
         voluptate?
       </p>
+      <Button name="hello" size={ButtonSize.xs} />
+      <Button name="hello" size={ButtonSize.sm} />
+      <Button name="hello" />
+      <Button name="hello" size={ButtonSize.lg} />
+      <Button name="hello" size={ButtonSize.xl} />
       <Image src={cat} size={ImageSize.xs} shape={ImageShape.ellipse} />
       <Image src={cat} size={ImageSize.sm} shape={ImageShape.ellipse} />
       <Image src={cat} size={ImageSize.md} shape={ImageShape.ellipse} />
       <Image src={cat} size={ImageSize.lg} shape={ImageShape.ellipse} />
       <br />
-      <Image src={cat} size={ImageSize.xs} />
+      <Img.Image src={cat} size={ImageSize.xs} />
       <Image src={cat} size={ImageSize.sm} />
       <Image src={cat} size={ImageSize.md} />
       <Image src={cat} size={ImageSize.lg} />
