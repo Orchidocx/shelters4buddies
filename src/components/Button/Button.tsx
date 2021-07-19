603086d96b7052aa6c73e045bf6c4b21833d6407
Jacky Li <ilikemacaronii@gmail.com>
2021-07-18 21:55:26 -0700

    added basic properties for button

--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,13 +1,31 @@
 import React from "react";
+import "./Button.css";
 
 type ButtonProps = {
   name: string;
+  size?: Number;
+  shape?: Number;
 };
 
-const Button = ({ name }: any) => (
-  <div className="button">
-    <h1>Hello button</h1>
-  </div>
+enum ButtonSize {
+  xs,
+  sm,
+  md,
+  lg,
+  xl,
+}
+
+enum ButtonShape {
+  default, // no border-radius
+  pill, // rounded; border-radius approximately ~15%
+}
+
+const Button = ({
+  name = "Button",
+  size = ButtonSize.md,
+  shape = ButtonShape.default,
+}: ButtonProps) => (
+  <div className={`button buttonSize${size} buttonShape${shape}`}>{name}</div>
 );
 
-export default Button;
+export { Button, ButtonSize, ButtonShape };
