917943ed41ce0e22c19caade12d56b69790c9253
Jacky Li <ilikemacaronii@gmail.com>
2021-07-20 01:02:56 -0700

    changed sample text to "Learn More"

--- src/views/Home/Home.tsx
+++ src/views/Home/Home.tsx
@@ -19,16 +19,16 @@ function Home() {
         dolore aperiam maxime ducimus dolor vel quibusdam unde numquam nostrum
         voluptate?
       </p>
-      <Button name="hello" size={ButtonSize.xs} />
-      <Button name="hello" size={ButtonSize.sm} />
-      <Button name="hello" />
-      <Button name="hello" size={ButtonSize.lg} />
-      <Button name="hello" size={ButtonSize.xl} />
-      <Button name="hello" size={ButtonSize.xs} shape={ButtonShape.pill} />
-      <Button name="hello" size={ButtonSize.sm} shape={ButtonShape.pill}/>
-      <Button name="hello"shape={ButtonShape.pill} />
-      <Button name="hello" size={ButtonSize.lg} shape={ButtonShape.pill}/>
-      <Button name="hello" size={ButtonSize.xl} shape={ButtonShape.pill} />
+      <Button name="Learn More" size={ButtonSize.xs} />
+      <Button name="Learn More" size={ButtonSize.sm} />
+      <Button name="Learn More" />
+      <Button name="Learn More" size={ButtonSize.lg} />
+      <Button name="Learn More" size={ButtonSize.xl} />
+      <Button name="Learn More" size={ButtonSize.xs} shape={ButtonShape.pill} />
+      <Button name="Learn More" size={ButtonSize.sm} shape={ButtonShape.pill} />
+      <Button name="Learn More" shape={ButtonShape.pill} />
+      <Button name="Learn More" size={ButtonSize.lg} shape={ButtonShape.pill} />
+      <Button name="Learn More" size={ButtonSize.xl} shape={ButtonShape.pill} />
       <Image src={cat} size={ImageSize.xs} shape={ImageShape.ellipse} />
       <Image src={cat} size={ImageSize.sm} shape={ImageShape.ellipse} />
       <Image src={cat} size={ImageSize.md} shape={ImageShape.ellipse} />
